51dd1512ffd7f38d8e23749626f49bb192b84679
KaneM2 <61199367+KaneM2@users.noreply.github.com>
2020-02-23 15:21:56 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from distutils.core import setup
 setup(
     name='RLProject',
     version='0.1',
-    packages=[''],
-    url='',
-    license='',
+    packages=['gym'],
     author='MPKan',
     author_email='kanem2@tcd.ie',
     description='Project in Reinforcement Learning for Mathematics undergrad'
