a14f9dd5aaada749b18f3c3f05e61267d50a1e4c
KaneM2 <61199367+KaneM2@users.noreply.github.com>
2020-02-27 12:15:30 +0000

    Create gridTD.py
    Contains code for render TD value function of random policy
    Plots TD estimate of state [0,4]

---
+++ Gridworld/gridTD.py
@@ -0,0 +1,55 @@
+from Gridworld.gridworld import *
+if __name__ == '__main__':
+    n_games = 75000
+    ALPHA = 0.01
+    count = 1
+
+    totalRewards = np.zeros(n_games)
+    rDict = {
+        3: 600,
+        21: -300,
+        33: -50,
+        49: -300,
+        55: -100
+    }
+
+    environment = Gridworld(8, 8, rDict)
+    environment.render()
+
+    v = [0 for i in range(len(environment.allStates))]  # Initialise value function to 0
+    valueFunctionInstance = []
+
+    for i in range(n_games):
+        finished = False
+        episodeRewards = 0
+        currentObservation = environment.reset()
+
+        if count % 10000 == 0:
+            print('Episode ', count)
+        num_steps = 0
+        while not finished:
+            obs = currentObservation
+            oldValue = v[obs]
+            sampledAction = environment.sampleRandomAction()
+            currentObservation, sampledReward, finished, info = environment.step(sampledAction)
+            episodeRewards += sampledReward
+            v[obs] += ALPHA * (sampledReward + v[currentObservation] - oldValue)
+
+        totalRewards[i] += episodeRewards
+        count += 1
+
+        valueFunctionInstance.append(v[24])
+
+    plt.plot(totalRewards)
+    plt.title('Episode Rewards for random policy')
+    plt.show()
+
+    renderValueFunction(v, 8, 8)
+
+    plt.plot(valueFunctionInstance)
+    plt.title('TD estimate of state [0,4]')
+    plt.ylabel('v(s)')
+    plt.xlabel('Episode')
+    plt.show()
+
+
