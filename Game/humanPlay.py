613addc8b16e702480d7454830317a84bb3bcc20
KaneM2 <61199367+KaneM2@users.noreply.github.com>
2020-03-04 20:51:09 +0000

    Minor changes
    Minor changes

--- Game/humanPlay.py
+++ Game/humanPlay.py
@@ -2,28 +2,42 @@ from Game.snakeCore import *
 import time
 
 if __name__ == '__main__':
-    env = Environment(20, 500, 10, 10)
+    env = Environment(20, 500, 10, 10,10)
     finished = False
-    action = env.snake.direction
+    offGridStarts = 0
 
-    while not finished:
-        time.sleep(0.08)  # Make the game slow down
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                finished = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    action = 1
-                if event.key == pygame.K_LEFT:
-                    action = 0
-                if event.key == pygame.K_UP:
-                    action = 2
-                if event.key == pygame.K_DOWN:
-                    action = 3
-        env.step(action)
+    for i in range(1000):
+        action = env.reset()
+        totalReward = 0
         if env.isOffGrid():
-            pygame.quit()
-            quit()
+            action = env.reset()
 
-        env.render()
+        if env.isOffGrid():
+            print(env.snake.blocks)
+            offGridStarts += 1
+        finished = False
+        time.sleep(1)
+        while not finished:
+            time.sleep(0.08)
+            for event in pygame.event.get():
+
+                if event.type == pygame.QUIT:
+                    finished = True
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        action = 1
+                    if event.key == pygame.K_LEFT:
+                        action = 0
+                    if event.key == pygame.K_UP:
+                        action = 2
+                    if event.key == pygame.K_DOWN:
+                        action = 3
+
+            state, stepReward, finished, info = env.step(action)
+            env.render()
+
+            totalReward += stepReward
+        if i % 100 == 0:
+            print('Game', i)
+    print('Fraction of off grid starts', offGridStarts / 10000)
+    pygame.quit()
--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from distutils.core import setup
 setup(
     name='RLProject',
     version='0.1',
-    packages=['gym'],
     author='MPKan',
     author_email='kanem2@tcd.ie',
     description='Project in Reinforcement Learning for Mathematics undergrad'
